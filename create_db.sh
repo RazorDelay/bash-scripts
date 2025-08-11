4b60f9a9ac65492443fde3cfc4c9c8498d8848db
RazorDelay <139871760+RazorDelay@users.noreply.github.com>
2025-08-11 21:26:13 +0300

    Update create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -1,25 +1,3 @@
-#!/bin/bash
 
-
-DATABASE_FILE="passwords.db"
-
-
-SQL_COMMANDS="CREATE TABLE entries (
-    id  INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL,
-    );"
-
-
- if [ -e "$DATABASE_FILE" ]; then
-     echo "Database already exists"
-     exit 0
- fi
- 
- if sqlite3 "$DATABASE_FILE" "$SQL_COMMANDS"; then
-       echo "Database is up"
- else
-       echo "[!] ERROR: task failed"
- fi
  
         
