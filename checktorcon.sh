be2830aa0dd64d24e7c5c96884a55c2dd304ca61
RazorDelay <139871760+RazorDelay@users.noreply.github.com>
2025-09-14 15:51:28 +0300

    Delete checktorcon.sh

--- checktorcon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-tor=$(curl --socks5-hostname localhost:9050 -s https://check.torproject.org > /dev/null; echo $?)
-[ "$tor" -gt 0 ] && echo " [!] check your tor connection." || echo " [+] under tor network."
